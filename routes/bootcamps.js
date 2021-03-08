cbe156b71113ba0914e36966dd0d9aae100047e8
LucasAlek <lucas.alek@icarotech.com>
2021-03-08 08:54:34 -0300

    criando controllers e simplicando as rotas ~atenção às rotas~

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,33 +1,22 @@
 const express = require("express");
-const router = express.Router();
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamp");
 
+const router = express.Router();
 // Não é mais necessário usar /api/v1/bootcamps
 // pois estamos montando a rota (lá no server.js) já com esse prefixo
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get single bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
